f9bf9d8d76873448d9735d4771a868e7fe20d4d3
bhupendar007k <bhupendar.bhandari@thewitslab.com>
2022-01-27 18:20:06 +0530

    initial commit

---
+++ task3.js
@@ -0,0 +1,82 @@
+class Node {
+    // constructor
+    constructor(element) {
+        this.element = element;
+        this.next = null
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.size = 0;
+    }
+
+
+    add(element) {
+        // creates a new node
+        var node = new Node(element);
+
+        // to store current node
+        var current;
+
+        // if list is Empty add the
+        // element and make it head
+        if (this.head == null)
+            this.head = node;
+        else {
+            current = this.head;
+
+            // iterate to the end of the
+            // list
+            while (current.next) {
+                current = current.next;
+            }
+
+            // add node
+            current.next = node;
+        }
+        this.size++;
+    }
+    index(element) {
+        var count = 0;
+        var current = this.head;
+
+        // iterate over the list
+        while (current != null) {
+            // compare each element of the list
+            // with given element
+            if (current.element === element)
+                return count;
+            count++;
+            current = current.next;
+        }
+
+        // not found
+        return -1;
+    }
+    printList() {
+        var curr = this.head;
+        var str = "";
+        while (curr) {
+            str += curr.element + " ";
+            curr = curr.next;
+        }
+        console.log(str);
+    }
+}
+let linkedlist = new LinkedList();
+linkedlist.add(5);
+linkedlist.add(28);
+linkedlist.add(99);
+linkedlist.add(67);
+linkedlist.add(1111);
+linkedlist.add(3);
+linkedlist.add(9999);
+linkedlist.add(8901);
+linkedlist.add(491);
+linkedlist.add(123);
+linkedlist.add(69);
+
+linkedlist.printList();
+console.log(linkedlist.index(10));
